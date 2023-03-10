73637e8f1ff0bc9808ba2535bfa10f8cd47a946d
Gaaaamma <ytching552022@gmail.com>
2023-03-10 20:57:23 +0800

    Fix: simpleInference - Finf with api calculation timestamp_300_2

--- simpleInference/SimpleInferenceTopology.java
+++ simpleInference/SimpleInferenceTopology.java
@@ -14,12 +14,12 @@ import org.apache.storm.tuple.Fields;
         builder.setSpout("ecgdataSpout", new EcgdataSpout(), 1);
         // builder.setBolt("migrpcBolt", new MiGrpcBolt(), 1).shuffleGrouping("ecgdataSpout");
         // builder.setBolt("miinfBolt", new MiInfBolt(), 1).shuffleGrouping("ecgdataSpout");
-        builder.setBolt("miinfBolt", new MiInfBolt(), 4).fieldsGrouping("ecgdataSpout", new Fields("patientID"));
+        builder.setBolt("miinfBolt", new MiInfBolt(), 2).fieldsGrouping("ecgdataSpout", new Fields("patientID"));
         // builder.setBolt("inferenceBolt", new InferenceBolt(), 1).shuffleGrouping("ecgdataSpout");
         builder.setBolt("storeBolt", new StoreBolt(), 1).shuffleGrouping("miinfBolt");
 
         conf.setDebug(false);
-        conf.setNumWorkers(6);
+        conf.setNumWorkers(4);
 
         String topologyName = "SimpleInferenceTopology";
         return submit(topologyName, conf, builder);
