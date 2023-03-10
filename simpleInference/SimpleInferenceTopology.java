37ec1d18298803e4e96c0abe2719a390132e83f7
Gaaaamma <ytching552022@gmail.com>
2023-03-10 17:08:53 +0800

    Add: simpleInference - Finf with api calculation timestamp_100_1

--- simpleInference/SimpleInferenceTopology.java
+++ simpleInference/SimpleInferenceTopology.java
@@ -1,6 +1,6 @@
 import org.apache.storm.topology.ConfigurableTopology;
 import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.tuple.Fields;
+// import org.apache.storm.tuple.Fields;
 
  public class SimpleInferenceTopology extends ConfigurableTopology {
 
@@ -13,12 +13,13 @@ import org.apache.storm.tuple.Fields;
 
         builder.setSpout("ecgdataSpout", new EcgdataSpout(), 1);
         // builder.setBolt("migrpcBolt", new MiGrpcBolt(), 1).shuffleGrouping("ecgdataSpout");
-        builder.setBolt("miinfBolt", new MiInfBolt(), 2).fieldsGrouping("ecgdataSpout", new Fields("patientID"));
+        builder.setBolt("miinfBolt", new MiInfBolt(), 1).shuffleGrouping("ecgdataSpout");
+        // builder.setBolt("miinfBolt", new MiInfBolt(), 2).fieldsGrouping("ecgdataSpout", new Fields("patientID"));
         // builder.setBolt("inferenceBolt", new InferenceBolt(), 1).shuffleGrouping("ecgdataSpout");
         builder.setBolt("storeBolt", new StoreBolt(), 1).shuffleGrouping("miinfBolt");
 
         conf.setDebug(false);
-        conf.setNumWorkers(4);
+        conf.setNumWorkers(3);
 
         String topologyName = "SimpleInferenceTopology";
         return submit(topologyName, conf, builder);
--- simpleInference/resources/miInfServerBolt.py
+++ simpleInference/resources/miInfServerBolt.py
@@ -7,6 +7,7 @@ from keras.models import load_model
 from keras.optimizers import Adam
 import Functions as F
 import sys
+import requests
 
 #******************************************#
 #             global definition            #
@@ -16,6 +17,11 @@ SEG_LEN = 10                    # 資料長度(秒)
 SAMPLE_RATE = 256               # 資料取樣率(點/秒)
 TOLERANCE =5                    # check_length tolerance
 MAX_MESSAGE_LENGTH = 256*1024*1024 # (Byte) equals to 256 MB
+
+STORM_TIMESTAMP_START_API = "http://140.113.170.152:32777/storm/timestamp/MI_INF_START"
+STORM_TIMESTAMP_DONE_API = "http://140.113.170.152:32777/storm/timestamp/MI_INF_DONE."
+PATIENT_START = "NCTU0000_0"
+PATIENT_END = "NCTU0002_32"
 
 # Load model
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -232,6 +238,8 @@ def doInference(request):
 class MiInfServerBolt(storm.BasicBolt):
     def process(self, tup):
         patientID = tup.values[0]
+        if (patientID == PATIENT_START):
+            response = requests.get(STORM_TIMESTAMP_START_API)
         seconds = tup.values[1]
         t1 = tup.values[2]
         t1d1 = tup.values[3]
@@ -295,6 +303,8 @@ class MiInfServerBolt(storm.BasicBolt):
         
         input = Input(patientID, [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10])
         result = doInference(input)
+        if (patientID == PATIENT_END):
+            response = requests.get(STORM_TIMESTAMP_DONE_API)
         storm.emit([patientID, SYMPTOM, result])
         
 MiInfServerBolt().run()
