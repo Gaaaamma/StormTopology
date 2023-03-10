22f9100a1a33b9b27176be78da5907ff2d5241c3
Gaaaamma <ytching552022@gmail.com>
2023-03-10 17:58:14 +0800

    Add: statistics - statistics_cost to analysize the avg latency

---
+++ statistics/statistics_cost.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+FILE_NAME = "FInf_100_1_1.txt"
+
+# Open file
+fp = open(FILE_NAME, "r")
+line = fp.readline()
+ 
+index = 1
+getEcgTime = 0
+getStoreDone = 0
+diff = []
+
+# Parsing data
+while line:
+    split = line.split(": ")
+    if (split[0] == "GET_ECG_DATA"):
+        getEcgTime = int(split[1])
+    
+    if (split[0] == "MI_STO_DONE."):
+        getStoreDone = int(split[1])
+        print(index, end = ": ")
+        print(getEcgTime, end = ", ")
+        print(getStoreDone, end = " => ")
+        print(getStoreDone - getEcgTime, end = "ms\n")
+        diff.append(getStoreDone - getEcgTime)
+        index += 1
+
+    line = fp.readline()
+
+# Generate report
+maxLatency = 0
+minLatency = 100000
+sum = 0
+for num in diff:
+    if (num > maxLatency):
+        maxLatency = num
+    if (num < minLatency):
+        minLatency = num
+    sum += num
+
+avg = Decimal(sum / len(diff)).quantize(Decimal('1.00'))
+print("\nReport: " + FILE_NAME)
+print("Round: " + str(len(diff)))
+print("AvgLatency: " + str(avg) + "ms")
+print("MinLatency: " + str(minLatency) + "ms")
+print("MaxLatency: " + str(maxLatency) + "ms\n")
+
+fp.close()
